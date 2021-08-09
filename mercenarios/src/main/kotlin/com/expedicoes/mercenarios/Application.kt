6f165bf0c90ff4a28d6e78cb4b0a989579092a17
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2021-08-08 23:51:41 -0300

    adiciona aplicação

---
+++ mercenarios/src/main/kotlin/com/expedicoes/mercenarios/Application.kt
@@ -0,0 +1,11 @@
+package com.expedicoes.mercenarios
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
